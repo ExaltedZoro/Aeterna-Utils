d53578c11b943949b2609f2b32861e07dc73db30
ExaltedZoro <98353510+ExaltedZoro@users.noreply.github.com>
2023-11-01 23:38:36 +0000

    Add Eldritch item

--- src/main/java/exaltedzoro/aeternautils/item/ModItems.java
+++ src/main/java/exaltedzoro/aeternautils/item/ModItems.java
@@ -1,4 +1,20 @@
 package exaltedzoro.aeternautils.item;
 
+import exaltedzoro.aeternautils.AeternaUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AeternaUtils.MOD_ID);
+
+    public static final RegistryObject<Item> ELDRITCH = ITEMS.register("eldritch", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
