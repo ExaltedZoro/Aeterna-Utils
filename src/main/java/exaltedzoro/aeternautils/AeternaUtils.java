531fae6c12229a9bed3aa025b3cf83799644edc1
ExaltedZoro <98353510+ExaltedZoro@users.noreply.github.com>
2023-11-01 23:40:43 +0000

    Added Item registry

--- src/main/java/exaltedzoro/aeternautils/AeternaUtils.java
+++ src/main/java/exaltedzoro/aeternautils/AeternaUtils.java
@@ -1,6 +1,7 @@
 package exaltedzoro.aeternautils;
 
 import com.mojang.logging.LogUtils;
+import exaltedzoro.aeternautils.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,6 +23,8 @@ public class AeternaUtils
     public AeternaUtils()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
