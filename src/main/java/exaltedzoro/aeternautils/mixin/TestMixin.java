403facd989691a6a7f3aaab359ddac2747320469
ExaltedZoro <98353510+ExaltedZoro@users.noreply.github.com>
2023-11-01 23:39:08 +0000

    Remove TestMixin

--- src/main/java/exaltedzoro/aeternautils/mixin/TestMixin.java
+++
@@ -1,8 +0,0 @@
-package exaltedzoro.aeternautils.mixin;
-
-import net.minecraft.world.entity.item.ItemEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemEntity.class)
-public class TestMixin {
-}
