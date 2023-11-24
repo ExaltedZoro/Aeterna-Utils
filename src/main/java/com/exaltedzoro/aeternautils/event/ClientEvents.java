77d06ce8bf9ccb32c4391f87536840fe0d6f5e39
ExaltedZoro <98353510+ExaltedZoro@users.noreply.github.com>
2023-11-24 00:36:30 +0000

    Added ClientEvents class

---
+++ src/main/java/com/exaltedzoro/aeternautils/event/ClientEvents.java
@@ -0,0 +1,24 @@
+package com.exaltedzoro.aeternautils.event;
+
+import com.exaltedzoro.aeternautils.AeternaUtils;
+import com.exaltedzoro.aeternautils.block.entity.ModBlockEntities;
+import com.exaltedzoro.aeternautils.block.entity.renderer.BeyondAltarBlockEntityRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = AeternaUtils.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = AeternaUtils.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+            event.registerBlockEntityRenderer(ModBlockEntities.BEYOND_ALTAR.get(), BeyondAltarBlockEntityRenderer::new);
+        }
+    }
+}
