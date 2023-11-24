1a31a5343c6f07b8c6c57a76693cedc7b991acbb
ExaltedZoro <98353510+ExaltedZoro@users.noreply.github.com>
2023-11-24 00:32:24 +0000

    Added ModMessages

---
+++ src/main/java/com/exaltedzoro/aeternautils/networking/ModMessages.java
@@ -0,0 +1,48 @@
+package com.exaltedzoro.aeternautils.networking;
+
+import com.exaltedzoro.aeternautils.AeternaUtils;
+import com.exaltedzoro.aeternautils.networking.packet.SyncItemStackToClientPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+public class ModMessages {
+    private static SimpleChannel INSTANCE;
+    private static int packetID = 0;
+
+    private static int id() {
+        return packetID++;
+    }
+
+    public static void register() {
+        SimpleChannel net = NetworkRegistry.ChannelBuilder
+                .named(new ResourceLocation(AeternaUtils.MOD_ID, "messages"))
+                .networkProtocolVersion(() -> "1.0")
+                .clientAcceptedVersions(s -> true)
+                .serverAcceptedVersions(s -> true)
+                .simpleChannel();
+
+        INSTANCE = net;
+
+        net.messageBuilder(SyncItemStackToClientPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(SyncItemStackToClientPacket::new)
+                .encoder(SyncItemStackToClientPacket::toBytes)
+                .consumerMainThread(SyncItemStackToClientPacket::handle)
+                .add();
+    }
+
+    public static <MSG> void sendToServer(MSG message) {
+        INSTANCE.sendToServer(message);
+    }
+
+    public static <MSG> void sendToPlayer(MSG message, ServerPlayer player) {
+        INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), message);
+    }
+
+    public static <MSG> void sendToClients(MSG message) {
+        INSTANCE.send(PacketDistributor.ALL.noArg(), message);
+    }
+}
