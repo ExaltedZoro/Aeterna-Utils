3fa675f41b62c42516e70e37ea99ddcafee51527
ExaltedZoro <98353510+ExaltedZoro@users.noreply.github.com>
2023-12-11 12:05:35 +0000

    Added SingularityJarBlockEntityRenderer

---
+++ src/main/java/com/exaltedzoro/aeternautils/block/entity/renderer/SingularityJarBlockEntityRenderer.java
@@ -0,0 +1,17 @@
+package com.exaltedzoro.aeternautils.block.entity.renderer;
+
+import com.exaltedzoro.aeternautils.block.entity.SingularityJarBlockEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+
+public class SingularityJarBlockEntityRenderer implements BlockEntityRenderer<SingularityJarBlockEntity> {
+    public SingularityJarBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
+
+    }
+    @Override
+    public void render(SingularityJarBlockEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
+
+    }
+}
