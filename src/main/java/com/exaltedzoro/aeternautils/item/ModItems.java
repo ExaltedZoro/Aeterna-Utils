e3f5bf9e75267c74ec5f0ac692ca9ae15d5ad316
ExaltedZoro <njermyj5@gmail.com>
2023-12-20 00:03:37 +0000

    Added Source Bowl item

--- src/main/java/com/exaltedzoro/aeternautils/item/ModItems.java
+++ src/main/java/com/exaltedzoro/aeternautils/item/ModItems.java
@@ -1,16 +1,20 @@
 package com.exaltedzoro.aeternautils.item;
 
 import com.exaltedzoro.aeternautils.AeternaUtils;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AeternaUtils.MOD_ID);
 
     //public static final RegistryObject<Item> ITEM_NAME = ITEMS.register("itemID", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> SOURCE_BOWL = ITEMS.register("source_bowl", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
