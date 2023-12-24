411c4347d7304e2c232f295bc2d355438848698e
ExaltedZoro <njermyj5@gmail.com>
2023-12-24 17:29:36 +0000

    Removed a single empty line lol

--- src/main/java/com/exaltedzoro/aeternautils/compat/kubejs/BeyondAltarRecipeJS.java
+++ src/main/java/com/exaltedzoro/aeternautils/compat/kubejs/BeyondAltarRecipeJS.java
@@ -8,7 +8,6 @@ import dev.latvian.mods.kubejs.recipe.component.NumberComponent;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
 
 public interface BeyondAltarRecipeJS {
-
     RecipeKey<OutputItem> OUTPUT = ItemComponents.OUTPUT.key("output");
     RecipeKey<InputItem> INPUT = ItemComponents.INPUT.key("input");
     RecipeKey<InputItem[]> PEDESTAL_ITEMS = ItemComponents.INPUT_ARRAY.key("pedestalItems");
