e5634e7fd663a38a85890e445b9f88d86bfc3b3c
ExaltedZoro <njermyj5@gmail.com>
2024-01-23 00:04:16 +0000

    Added models for Solar and Lunar Sourcelinks

--- src/main/java/com/exaltedzoro/aeternautils/block/SolarSourcelinkBlock.java
+++ src/main/java/com/exaltedzoro/aeternautils/block/SolarSourcelinkBlock.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -22,7 +23,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class SolarSourcelinkBlock extends BaseEntityBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-    private static final VoxelShape SHAPE = Shapes.box(0, 0, 0, 1, 1, 1);
+    private static final VoxelShape SHAPE = makeShape();
     public SolarSourcelinkBlock(Properties properties) {
         super(properties);
     }
@@ -30,6 +31,35 @@ public class SolarSourcelinkBlock extends BaseEntityBlock {
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
+    }
+
+    public static VoxelShape makeShape() {
+        VoxelShape shape = Shapes.empty();
+
+        //Body
+        shape = Shapes.join(shape, box(4, 4, 4, 12, 12, 12), BooleanOp.OR);
+
+        //North support
+        shape = Shapes.join(shape, box(7, 12, 0, 9, 16, 3), BooleanOp.OR);
+        shape = Shapes.join(shape, box(7, 1, 3, 9, 15, 4), BooleanOp.OR);
+        shape = Shapes.join(shape, box(7, 0, 0, 9, 4, 3), BooleanOp.OR);
+
+        //South support
+        shape = Shapes.join(shape, box(7, 12, 13, 9, 16, 16), BooleanOp.OR);
+        shape = Shapes.join(shape, box(7, 1, 12, 9, 15, 13), BooleanOp.OR);
+        shape = Shapes.join(shape, box(7, 0, 13, 9, 4, 16), BooleanOp.OR);
+
+        //East support
+        shape = Shapes.join(shape, box(13, 12, 7, 16, 16, 9), BooleanOp.OR);
+        shape = Shapes.join(shape, box(12, 1, 7, 13, 15, 9), BooleanOp.OR);
+        shape = Shapes.join(shape, box(13, 0, 7, 16, 4, 9), BooleanOp.OR);
+
+        //West support
+        shape = Shapes.join(shape, box(0, 12, 7, 3, 16, 9), BooleanOp.OR);
+        shape = Shapes.join(shape, box(3, 1, 7, 4, 15, 9), BooleanOp.OR);
+        shape = Shapes.join(shape, box(0, 0, 7, 3, 4, 9), BooleanOp.OR);
+
+        return shape;
     }
 
     @Override
